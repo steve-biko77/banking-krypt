4ad25ef25d5e4219dae8149c74a108c914486ab2
steve-biko77 <wkstevebiko1@gmail.com>
2025-12-13 14:20:37 +0100

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+    SIDEBAR
+    {children}
+    </main>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopack: false,   // ← désactive Turbopack
+  },
 };
 
 export default nextConfig;
